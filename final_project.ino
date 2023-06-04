07ec76516998291ebe72b6c6e09645dea6821df3
= <=>
2023-06-04 12:28:32 -0700

    removed commented out code

--- final_project.ino
+++ final_project.ino
@@ -40,7 +40,7 @@
 #include <CayenneLPP.h>
 
 #define DHTPIN 12
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
 
 static const uint32_t GPSBaud = 9600;
 
@@ -54,19 +54,19 @@ CayenneLPP lpp(51);
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]={ 0x90, 0x60, 0xC5, 0x98, 0x46, 0xF9, 0x81, 0x60 };
-void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+static const u1_t PROGMEM APPEUI[8] = {0x90, 0x60, 0xC5, 0x98, 0x46, 0xF9, 0x81, 0x60};
+void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={ 0x54, 0xB5, 0x33, 0xF9, 0x43, 0xF9, 0x81, 0x60 };
-void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+static const u1_t PROGMEM DEVEUI[8] = {0x54, 0xB5, 0x33, 0xF9, 0x43, 0xF9, 0x81, 0x60};
+void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
 // The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = { 0x43, 0x4F, 0x16, 0x25, 0xD9, 0xB9, 0xA9, 0x9A, 0x7E, 0x36, 0x7F, 0xA1, 0x17, 0x58, 0xDB, 0xDC };
-void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+static const u1_t PROGMEM APPKEY[16] = {0x43, 0x4F, 0x16, 0x25, 0xD9, 0xB9, 0xA9, 0x9A, 0x7E, 0x36, 0x7F, 0xA1, 0x17, 0x58, 0xDB, 0xDC};
+void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
 
 static osjob_t sendjob;
 
@@ -82,94 +82,103 @@ const lmic_pinmap lmic_pins = {
     .dio = {3, 6, 2},
 };
 
-void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch(ev) {
-        case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
-            break;
-        case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
-            break;
-        case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
-            break;
-        case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
-            break;
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
+void onEvent(ev_t ev)
+{
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev)
+  {
+  case EV_SCAN_TIMEOUT:
+    Serial.println(F("EV_SCAN_TIMEOUT"));
+    break;
+  case EV_BEACON_FOUND:
+    Serial.println(F("EV_BEACON_FOUND"));
+    break;
+  case EV_BEACON_MISSED:
+    Serial.println(F("EV_BEACON_MISSED"));
+    break;
+  case EV_BEACON_TRACKED:
+    Serial.println(F("EV_BEACON_TRACKED"));
+    break;
+  case EV_JOINING:
+    Serial.println(F("EV_JOINING"));
+    break;
+  case EV_JOINED:
+    Serial.println(F("EV_JOINED"));
 
-            // Disable link check validation (automatically enabled
-            // during join, but not supported by TTN at this time).
-            LMIC_setLinkCheckMode(0);
-            break;
-        case EV_RFU1:
-            Serial.println(F("EV_RFU1"));
-            break;
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
-            if (LMIC.dataLen) {
-              Serial.println(F("Received "));
-              Serial.println(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
-            }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
-            break;
-        case EV_RESET:
-            Serial.println(F("EV_RESET"));
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-        case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
-            break;
-        case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
-            break;
-         default:
-            Serial.println(F("Unknown event"));
-            break;
+    // Disable link check validation (automatically enabled
+    // during join, but not supported by TTN at this time).
+    LMIC_setLinkCheckMode(0);
+    break;
+  case EV_RFU1:
+    Serial.println(F("EV_RFU1"));
+    break;
+  case EV_JOIN_FAILED:
+    Serial.println(F("EV_JOIN_FAILED"));
+    break;
+  case EV_REJOIN_FAILED:
+    Serial.println(F("EV_REJOIN_FAILED"));
+    break;
+    break;
+  case EV_TXCOMPLETE:
+    Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+    if (LMIC.txrxFlags & TXRX_ACK)
+      Serial.println(F("Received ack"));
+    if (LMIC.dataLen)
+    {
+      Serial.println(F("Received "));
+      Serial.println(LMIC.dataLen);
+      Serial.println(F(" bytes of payload"));
     }
+    // Schedule next transmission
+    os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+    break;
+  case EV_LOST_TSYNC:
+    Serial.println(F("EV_LOST_TSYNC"));
+    break;
+  case EV_RESET:
+    Serial.println(F("EV_RESET"));
+    break;
+  case EV_RXCOMPLETE:
+    // data received in ping slot
+    Serial.println(F("EV_RXCOMPLETE"));
+    break;
+  case EV_LINK_DEAD:
+    Serial.println(F("EV_LINK_DEAD"));
+    break;
+  case EV_LINK_ALIVE:
+    Serial.println(F("EV_LINK_ALIVE"));
+    break;
+  default:
+    Serial.println(F("Unknown event"));
+    break;
+  }
 }
-static void smartDelay(unsigned long ms) {
+static void smartDelay(unsigned long ms)
+{
   unsigned long start = millis();
-  do {
+  do
+  {
     while (Serial1.available())
       gps.encode(Serial1.read());
   } while (millis() - start < ms);
 }
 
-static void printFloat(float val, bool valid, int len, int prec) {
-  if (!valid) {
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
     while (len-- > 1)
       Serial.print('*');
     Serial.print(' ');
-  } else {
+  }
+  else
+  {
     Serial.print(val, prec);
     int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1);  // . and -
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
     flen += vi >= 1000 ? 4 : vi >= 100 ? 3
-                           : vi >= 10  ? 2
+                         : vi >= 10    ? 2
                                        : 1;
     for (int i = flen; i < len; ++i)
       Serial.print(' ');
@@ -177,132 +186,107 @@ static void printFloat(float val, bool valid, int len, int prec) {
   smartDelay(0);
 }
 
-void do_send(osjob_t* j){
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
+void do_send(osjob_t *j)
+{
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND)
+  {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  }
+  else
+  {
 
-        // Clear Payload
-        lpp.reset();
-      
-        /*// Get temperature event and print its value.
-        sensors_event_t event;
-        dht.temperature().getEvent(&event);
-        if (isnan(event.temperature)) {
-          Serial.println(F("Error reading temperature!"));
-        }
-        else {
-          Serial.print(F("Temperature: "));
-          Serial.print(event.temperature);
-          Serial.println(F("°C"));
+    // Clear Payload
+    lpp.reset();
 
-          lpp.addTemperature(1, event.temperature);
-        }
-        // Get humidity event and print its value.
-        dht.humidity().getEvent(&event);
-        if (isnan(event.relative_humidity)) {
-          Serial.println(F("Error reading humidity!"));
-        }
-        else {
-          Serial.print(F("Humidity: "));
-          Serial.print(event.relative_humidity);
-          Serial.println(F("%"));
-
-          lpp.addRelativeHumidity(2, event.relative_humidity);
-        }
-        */
-        if (Serial1.available()){
-          gps.encode(Serial1.read());
-        }
-        float lat = gps.location.lat();
-        float lng = gps.location.lng();
-        float alt = gps.altitude.meters();
-        lpp.addGPS(1, lat, lng, alt);
-        // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, lpp.getBuffer(), lpp.getSize(), 0);
-        Serial.print(lat, 6); //Serial.print(val, prec);
-        Serial.print(",");
-        Serial.print(lng, 6);
-        Serial.print(",");
-        Serial.print(alt, 6);
-        Serial.print(",");
-        Serial.println(LMIC.opmode);
-        //Serial.print(",");
-        //printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
-        //Serial.print(",");
-        //printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
-        //Serial.println(",");
-
-        //Serial.println(lpp.getBuffer());
-        Serial.println(F("Packet queued"));
-    }
-    // Next TX is scheduled after TX_COMPLETE event.
-}
-
-void setup() {
-    Serial.begin(9600);
-    Serial1.begin(GPSBaud);
-    Serial.println(F("Starting"));
-
-    /*##ifdef VCC_ENABLE
-    // For Pinoccio Scout boards
-    pinMode(VCC_ENABLE, OUTPUT);
-    digitalWrite(VCC_ENABLE, HIGH);
-    delay(1000);
-    #endif*/
-
-    /*// Initialize DHT22.
-    dht.begin();
-    Serial.println(F("DHTxx Unified Sensor Example"));
-    // Print temperature sensor details.
-    sensor_t sensor;
-    dht.temperature().getSensor(&sensor);
-    Serial.println(F("------------------------------------"));
-    Serial.println(F("Temperature Sensor"));
-    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-    Serial.println(F("------------------------------------"));
-    // Print humidity sensor details.
-    dht.humidity().getSensor(&sensor);
-    Serial.println(F("Humidity Sensor"));
-    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-    Serial.println(F("------------------------------------"));
-    */
-    // LMIC init
-    os_init();
-    // Reset the MAC state. Session and pending data transfers will be discarded.
-    LMIC_reset();
-    
-    // allow much more clock error than the X/1000 default. See:
-    // https://github.com/mcci-catena/arduino-lorawan/issues/74#issuecomment-462171974
-    // https://github.com/mcci-catena/arduino-lmic/commit/42da75b56#diff-16d75524a9920f5d043fe731a27cf85aL633
-    // the X/1000 means an error rate of 0.1%; the above issue discusses using
-    // values up to 10%. so, values from 10 (10% error, the most lax) to 1000
-    // (0.1% error, the most strict) can be used.
-    LMIC_setClockError(1 * MAX_CLOCK_ERROR / 40);
-
-    // Sub-band 2 - Helium Network
-    LMIC_selectSubBand(1); // zero indexed 
-    LMIC_setLinkCheckMode(0);
-    LMIC_setDrTxpow(DR_SF7, 14);
-
-    // Start job (sending automatically starts OTAA too)
-    do_send(&sendjob);
-}
-
-void loop() {
-    if (Serial1.available()){
+    if (Serial1.available())
+    {
       gps.encode(Serial1.read());
     }
-    os_runloop_once();
+    float lat = gps.location.lat();
+    float lng = gps.location.lng();
+    float alt = gps.altitude.meters();
+    lpp.addGPS(1, lat, lng, alt);
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, lpp.getBuffer(), lpp.getSize(), 0);
+    Serial.print(lat, 6); // Serial.print(val, prec);
+    Serial.print(",");
+    Serial.print(lng, 6);
+    Serial.print(",");
+    Serial.print(alt, 6);
+    Serial.print(",");
+    Serial.println(LMIC.opmode);
+    Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(GPSBaud);
+  Serial.println(F("Starting"));
+
+  /*##ifdef VCC_ENABLE
+  // For Pinoccio Scout boards
+  pinMode(VCC_ENABLE, OUTPUT);
+  digitalWrite(VCC_ENABLE, HIGH);
+  delay(1000);
+  #endif*/
+
+  /*// Initialize DHT22.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  */
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  // allow much more clock error than the X/1000 default. See:
+  // https://github.com/mcci-catena/arduino-lorawan/issues/74#issuecomment-462171974
+  // https://github.com/mcci-catena/arduino-lmic/commit/42da75b56#diff-16d75524a9920f5d043fe731a27cf85aL633
+  // the X/1000 means an error rate of 0.1%; the above issue discusses using
+  // values up to 10%. so, values from 10 (10% error, the most lax) to 1000
+  // (0.1% error, the most strict) can be used.
+  LMIC_setClockError(1 * MAX_CLOCK_ERROR / 40);
+
+  // Sub-band 2 - Helium Network
+  LMIC_selectSubBand(1); // zero indexed
+  LMIC_setLinkCheckMode(0);
+  LMIC_setDrTxpow(DR_SF7, 14);
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+}
+
+void loop()
+{
+  if (Serial1.available())
+  {
+    gps.encode(Serial1.read());
+  }
+  os_runloop_once();
 }
